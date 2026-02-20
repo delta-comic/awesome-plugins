a367d387bcc2bb29d52b83c2eddab2b4b0841c32
wenxig <wenxinguo12@gmail.com>
2026-02-21 01:24:09 +0800

    feat: upsert

--- src/actions/upsert.ts
+++ src/actions/upsert.ts
@@ -1,7 +1,7 @@
 import { Command } from 'commander'
 
-import { decodedUpsertIssue } from '../helper/issue'
-import { getIssue } from '../helper/repo'
+import { decodedUpsertIssue, type ArchivePlugin } from '../helper/issue'
+import { closeIssue, getIssue } from '../helper/repo'
 const program = new Command('upsert')
 
 program
@@ -9,7 +9,29 @@ program
   .argument('<number>', 'issue的id')
   .action(async id => {
     const issue = (await getIssue(Number(id))).data
-    decodedUpsertIssue(issue.body ?? '')
+    const plugin = decodedUpsertIssue(issue.body ?? '')
+    if (!plugin) {
+      await closeIssue(issue, '不符合格式的内容')
+      return
+    }
+    const file = Bun.file(`../../pages/${plugin.id}.json`)
+
+    if (await file.exists()) {
+      const content: ArchivePlugin = await file.json()
+      const isOwner = content.author.includes(issue.user?.name ?? '__')
+      if (!isOwner) {
+        await closeIssue(issue, '不是该插件的作者之一, 无权修改')
+        return
+      }
+    }
+
+    await file.write(
+      JSON.stringify(<ArchivePlugin>{
+        author: plugin.authors.concat([issue.user?.name ?? '']).filter(Boolean),
+        download: plugin.download,
+        id: plugin.id
+      })
+    )
   })
 
 export default program
--- src/helper/issue.ts
+++ src/helper/issue.ts
@@ -1,3 +1,31 @@
 export const decodedUpsertIssue = (body: string) => {
-  console.log(body)
+  try {
+    const [id, download, authorStr] = body
+      .split('\n')
+      .filter(Boolean)
+      .map(v => v.trim())
+      .filter(v => !v.startsWith('###'))
+    if (!id) throw new RangeError('not found plugin id')
+    if (!download) throw new RangeError('not found plugin download')
+    if (!authorStr) throw new RangeError('not found plugin authorStr')
+
+    const authors =
+      authorStr == '_No response_'
+        ? []
+        : authorStr
+            .split(',')
+            .filter(Boolean)
+            .map(v => v.trim())
+    return { id, authors, download }
+  } catch (err) {
+    console.warn('fail to decode issue')
+    console.error(err)
+    return
+  }
+}
+
+export interface ArchivePlugin {
+  id: string
+  download: string
+  author: string[]
 }
